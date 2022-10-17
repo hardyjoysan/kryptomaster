74efdca8ba3991afe3c43a2856acf7ad1f63a08d
Hardy Joysan <hardyjoysan@gmail.com>
2022-10-17 14:37:26 +0530

    Smart contract deploy script added

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./transactions.json";
+
+const contractABI = abi.abi;
+const contractAddress = "0xE7fC99a8617c231b76Ce368f942f64E0181A1744";
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/EaiDXSmmC44xxy0dDEnzc-_s2aatWz-t",
+      accounts: [
+        "39aa0578844491a7527faf07eed7d796784893f7c2fb0920caeb6cf1d497958d",
+      ],
+    },
+  },
 };
